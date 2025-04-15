7cedaae9499930e7bc156be2d3e1d81faa87821a
vladkop2025 <vladkop@mail.ru>
2025-04-15 21:02:24 +0300

    Add project files.

---
+++ Algoritm_Sample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class BinarySearchExample
+{
+    // Метод бинарного поиска
+    public static int BinarySearch(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2; // Предотвращает переполнение
+
+            if (array[mid] == target)
+                return mid; // Элемент найден
+            else if (array[mid] < target)
+                left = mid + 1; // Ищем в правой половине
+            else
+                right = mid - 1; // Ищем в левой половине
+        }
+
+        return -1; // Элемент не найден
+    }
+
+    static void Main()
+    {
+        int[] sortedArray = { 1, 3, 5, 7, 9, 11, 13, 15 };
+        int target = 7;
+
+        int result = BinarySearch(sortedArray, target);
+
+        if (result != -1)
+            Console.WriteLine($"Элемент {target} найден на позиции {result}.");
+        else
+            Console.WriteLine($"Элемент {target} не найден в массиве.");
+    }
+}
---
+++ Algoritm_Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Algoritm_Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Algoritm_Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9dfb6421-4ae1-4247-bf78-afeaa6b8c911")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
